d3c97ca7748c55d91c36c5269a0c27ae807acdf7
Sushma <sushmajaladi9@gmail.com>
2023-11-13 15:16:33 +0100

    remove srting space commit

---
+++ RemoveStringSpace/README.md
@@ -0,0 +1,2 @@
+#DESCRIPTION:
+Write a function that removes the spaces from the string, then return the resultant string.
---
+++ RemoveStringSpace/index.js
@@ -0,0 +1,3 @@
+function noSpace(inputString) {
+    return inputString.replace(/\s/g, ''); // This function removes spaces from the input string.
+  }
